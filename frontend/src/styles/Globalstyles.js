b1e20827a63e210cb67178e6e0e633c1255b4dc9
Rebecca Sighed Paemurd <rebecca.paemurd@gmail.com>
2026-03-04 09:08:10 +0100

    style changes

--- frontend/src/styles/Globalstyles.js
+++ frontend/src/styles/Globalstyles.js
@@ -15,7 +15,7 @@ const GlobalStyles = createGlobalStyle`
     overflow-x: hidden;
     -webkit-font-smoothing: antialiased;
 
-    font-size: 18px;
+    font-size: 19px;
     
     @media (min-width: 768px) {
     font-size: 16px;
